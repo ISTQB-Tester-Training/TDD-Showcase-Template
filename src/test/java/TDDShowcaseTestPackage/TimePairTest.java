f7dcb95035ae8d59769cdaabe06aaacdc7816e1a
cziegaus <54708551+cziegaus@users.noreply.github.com>
2021-03-30 19:21:38 +0200

    Commit nach Step 1

---
+++ src/test/java/TDDShowcaseTestPackage/TimePairTest.java
@@ -0,0 +1,15 @@
+package TDDShowcaseTestPackage;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TimePairTest {
+
+    TimePair aTimePair = new TimePair();
+
+    @Test
+    public void timeDifferenceTest_1h () {
+        aTimePair.setTimeValues("10:00", "11:00");
+        assertEquals(1.0, aTimePair.getTimeDifference(), 0.0);
+    }
+}
